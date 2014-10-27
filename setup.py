2472b61e082eff5d9612c738fb66fee5fae1eccf
Alexander Borzunov <hxrussia@gmail.com>
2014-10-28 05:40:16 +0600

    Bump to v0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "cpmoptimize",
-    version = "0.1",
-    packages = find_packages(),
+    version = "0.2",
+    packages = ['cpmoptimize'],
     
     install_requires = ['byteplay>=0.2'],
 
@@ -16,4 +16,5 @@ setup(
     license = "MIT",
     keywords = "optimize matrix bytecode",
     url = "http://github.com/borzunov/cpmoptimize",
+    download_url = 'https://github.com/borzunov/cpmoptimize/archive/v0.2.tar.gz',
 )
